fd6b6512f0192cf0918c97322cff71c9a7d770d8
jackattack <jacquelinenicole@me.com>
2020-08-16 15:51:41 -0600

    Established variatbles for character types, established window confirms and window prompts

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-// Assignment Code
+// Establish window.prompt for how many characters user would like their password to be X
+// Validate password length inputted is between 8 and 128 characters X
+// Establish window.confirm for each possible password attribute 
+// If statements asssociated with each window prompt
+// create a variable containing array of special characters
+// create a cariable containing array of numbers 1-9
+// create a variable containing array of lowercase characters
+// create a varaiable containing array of uppercase characters
+// create generateBtn function
+// create writePassword function
+
+
+// ------------------ Global Variables ----------------------------
+
+
+
+var confirmUppercase = true;
+var confirmLowercase = true;
+var confirmSpecial = true;
+
+
+
+
+// array of special characters
+var specialCharacters = ["(", ")", "*", ":", "?", "$", "@", "/", "&", "{", "}", "!", "%", "~", "-", "_", "[", "]", "^"];
+// array of numbers 0-12
+var numericalCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+// array of lowercase letters
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+// array of uppercase letters
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -10,10 +43,23 @@ function writePassword() {
 
 }
 
+// ------------------- Functions ----------------------------------
 function generatePassword() {
-  return "password"
+  var userRequest = window.prompt("How many characters would you like your password to be? Please input a number between 8 and 128.");
+  if (isNaN(userRequest) || userRequest > 128 || userRequest < 8) {
+    window.alert("Please input a valid number.")
+  } else {
+    var confirmSpecial = window.confirm("Would you like your password to contain special characters?");
+    var confirmLowercase = window.confirm("Would you like your password to contain lowercase characters?");
+    var confirmUppercase = window.confirm("Would you like your password to contain uppercase characters?");
+    var confirmNumerical = window.confirm("Would you like your password to contain numbers?");
+  }
+if (confirmSpecial && confirmLowercase && confirmUppercase && confirmNumerical) {
+  userRequest = character.concat(specialCharacters, uppercaseLetters, lowercaseLetters, numericalCharacters);
+  }
 }
 
+// --------------- Main Processes/Event Listeners -----------------
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
